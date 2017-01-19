8070da40437729af8bf97fa7c5c74800b1f92616
Conner-Stack <connerstack@gmail.com>
2017-01-19 16:51:41 -0600

    push
    finite state machines, bro

---
+++ ConsoleApplication1/finite.state.machine/Program.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+public enum tLightState
+{
+    Init = 0,
+    Red = 1,
+    Green = 2,
+    Yellow = 3,
+    Exit = 9000,
+}
+
+namespace finite.state.machine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+        }
+    }
+}
